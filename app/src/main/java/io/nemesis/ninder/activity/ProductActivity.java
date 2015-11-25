cfdfc4bbd42fca27f542d3f46cf61ff7bedeb6f8
Boryana Miteva <boryana.miteva@tumba.com>
2015-11-25 10:52:58 +0200

    added product activity

---
+++ app/src/main/java/io/nemesis/ninder/activity/ProductActivity.java
@@ -0,0 +1,15 @@
+package io.nemesis.ninder.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import io.nemesis.ninder.R;
+
+public class ProductActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+}
